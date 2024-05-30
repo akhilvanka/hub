92be1f3b1dee10ff531615b922591b7b22a441b0
Akhil Vanka <dev+github@akhilv.me>
2024-05-30 16:43:37 -0400

    Clean json input for elasticsearch
    A QoL fix, removing unnecessary fields, and adding in a hash function
    to allow for quick comparison of the data, if case two jobs are the
    same but have different IDs.
    
    A docker-compose.yml file was also removed, as it was not needed for
    the project. Although credentials were leaked, this server no longer
    exists, and as such is not a problem.

--- lib/nexus/elasticsearch.ex
+++ lib/nexus/elasticsearch.ex
@@ -1,14 +1,29 @@
 defmodule Nexus.Elasticsearch do
+  @moduledoc """
+  A module for interacting with an Elasticsearch instance to store job data
+  """
 
   @es_url Application.compile_env!(:nexus, :elasticsearch_url)
   @es_user Application.compile_env!(:nexus, :elasticsearch_username)
   @es_pass Application.compile_env!(:nexus, :elasticsearch_password)
 
+  @doc """
+  Pass a Jason.decode!(body) of a company's job data to this function to store it in Elasticsearch
+  through a PUT request
+  """
+  @spec put(map) :: {:ok} | {:error, String.t}
   def put(job) do
-    ssl_config = ssl_config()
-    auth = basic_auth_header()
+    id = job["id"]
 
-    case HTTPoison.put("#{@es_url}/lever_jobs/_create/#{job["id"]}", Jason.encode!(job), [{"Content-Type", "application/json"} | auth], ssl: ssl_config) do
+    job = job
+          |> clean_job()
+          |> hash_job()
+
+    IO.inspect(job)
+    ssl_config = ssl_config()
+    auth = [{"Authorization", "Basic #{Base.encode64("#{@es_user}:#{@es_pass}")}"}]
+
+    case HTTPoison.put("#{@es_url}/lever_jobs/_create/#{id}", Jason.encode!(job), [{"Content-Type", "application/json"} | auth], ssl: ssl_config) do
       {:ok, %HTTPoison.Response{status_code: 201}} -> {:ok}
       {:ok, %HTTPoison.Response{status_code: 409}} -> {:error, "Job already exists"}
       {:error, %HTTPoison.Error{reason: reason}}   -> {:error, reason}
@@ -17,17 +32,24 @@ defmodule Nexus.Elasticsearch do
 
   defp ssl_config do
     cacertfile = "priv/certs/ca.crt"
-
     [
       verify: :verify_peer,
       cacertfile: cacertfile
     ]
   end
 
-  defp basic_auth_header do
-    encoded_credentials = Base.encode64("#{@es_user}:#{@es_pass}")
+  defp clean_job(job) do
+    job
+    |> Map.drop(["description", "descriptionBody", "opening", "openingPlain", "additional", "additionalPlain", "lists", "id"])
+    |> update_in(["categories"], fn
+      nil -> %{}
+      categories -> Map.drop(categories, ["location"])
+    end)
+  end
 
-    [{"Authorization", "Basic #{encoded_credentials}"}]
-  end 
-
+  defp hash_job(job) do
+    hash = :crypto.hash(:sha256, Jason.encode!(job))
+           |> Base.encode16()
+    Map.put(job, "hash", hash)
+  end
 end
