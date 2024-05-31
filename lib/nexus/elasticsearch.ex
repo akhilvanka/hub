b4974bfcb22f50f8ab8a66e189bff6d0fc2d883f
Akhil Vanka <dev+github@akhilv.me>
2024-05-31 09:00:12 -0400

    Remove warnings within elasticsearch
    When running the seeding program, the following warning was being
    displayed:
        `warning: Application.get_env/2 is discouraged in the module body,
        use Application.compile_env/3 instead`
    
    In order to follow better practice, its recommended to move all runtime
    environment variables to be within a function. This commit removes all
    the macros and instead places them where they are meant to be used (in
    lieu of the macro).

--- lib/nexus/elasticsearch.ex
+++ lib/nexus/elasticsearch.ex
@@ -3,10 +3,6 @@ defmodule Nexus.Elasticsearch do
   A module for interacting with an Elasticsearch instance to store job data
   """
 
-  @es_url Application.get_env(:nexus, :elasticsearch_url)
-  @es_user Application.get_env(:nexus, :elasticsearch_username)
-  @es_pass Application.get_env(:nexus, :elasticsearch_password)
-
   @doc """
   Pass a Jason.decode!(body) of a company's job data to this function to store it in Elasticsearch
   through a PUT request
@@ -20,9 +16,9 @@ defmodule Nexus.Elasticsearch do
           |> hash_job()
 
     ssl_config = ssl_config()
-    auth = [{"Authorization", "Basic #{Base.encode64("#{@es_user}:#{@es_pass}")}"}]
+    auth = [{"Authorization", "Basic #{Base.encode64("#{Application.get_env(:nexus, :elasticsearch_username)}:#{Application.get_env(:nexus, :elasticsearch_password)}")}"}]
 
-    case HTTPoison.put("#{@es_url}/lever_jobs/_create/#{id}", Jason.encode!(job), [{"Content-Type", "application/json"} | auth], ssl: ssl_config) do
+    case HTTPoison.put("#{Application.get_env(:nexus, :elasticsearch_url)}/lever_jobs/_create/#{id}", Jason.encode!(job), [{"Content-Type", "application/json"} | auth], ssl: ssl_config) do
       {:ok, %HTTPoison.Response{status_code: 201}} -> {:ok}
       {:ok, %HTTPoison.Response{status_code: 409}} -> {:error, "Job already exists"}
       {:error, %HTTPoison.Error{reason: reason}}   -> {:error, reason}
